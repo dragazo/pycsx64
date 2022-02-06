4dee7e7947726e20ec5146f822004f3c81bf15f4
Devin Jean <emailcruzjean@yahoo.com>
2022-02-06 00:43:16 -0600

    basic cpu access

---
+++ pytest/test.py
@@ -0,0 +1,38 @@
+import csx64
+
+def main():
+    prog_name = 'demo.asm'
+    prog = '''
+global main
+
+segment text
+main:
+    mov edi, 5
+    mov esi, 4
+    add edi, esi
+
+    mov eax, edi
+    ret
+'''
+    obj = csx64.assemble(prog_name, prog)
+    objs = csx64.stdlib()
+    objs.append((prog_name, obj))
+    exe = csx64.link(objs, ('start', 'main'))
+    emu = csx64.Emulator()
+    emu.init(exe)
+    _, state = emu.execute_cycles(0xffffffffffffffff)
+    assert state == 'Terminated' and emu.get_state() == 'Terminated' and emu.get_return_value() == 9
+
+    print(emu.rax, emu.raxi, emu.raxf, emu.flags, emu.cc_b)
+    emu.rax = 12
+    emu.iopl = 3
+    print(emu.rax, emu.raxi, emu.raxf, emu.flags, emu.cc_be)
+    emu.raxi = -14
+    emu.cf = True
+    emu.of = True
+    print(emu.rax, emu.raxi, emu.raxf, emu.flags, emu.cc_g)
+    emu.raxf = 12.43
+    print(emu.rax, emu.raxi, emu.raxf, emu.flags, emu.cc_ge)
+
+if __name__ == '__main__':
+    main()
